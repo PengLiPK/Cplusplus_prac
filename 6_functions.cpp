32144ebcd21aca329361d92c4593728ce10e3bf4
PengLiPK <00p000atgm@gmail.com>
2016-01-30 17:41:11 -0500

    Update functions

--- 6_functions.cpp
+++ 6_functions.cpp
@@ -18,6 +18,38 @@ void printmessage()
 	cout << "This is a void function!\n";
 }
 
+// access an external variable from within a function
+// arguments can be passed by reference, instead of by value
+void duplicate(int& a, int&b, int& c)
+{
+	a *= 2;
+	b *= 2;
+	c *= 2;
+}
+
+// default values in function
+int subtraction(int a, int b=2)
+{
+	int r;
+	r = a - b;
+	return r;
+}
+
+// declare functions prototype
+void odd(int x);
+
+// recursivity
+long factorial(long a)
+{
+	if(a>1)
+	{
+		return (a * factorial(a-1));
+	}
+	else
+	{
+		return 1;
+	}
+}
 
 int main ()
 {
@@ -32,5 +64,31 @@ int main ()
 
 	printmessage();
 
+
+	int xr=1, yr=3, zr=7;
+	duplicate(xr,yr,zr);
+	cout << "xr=" << xr << ", yr=" << yr << ", zr=" << zr << ".\n";
+
+	cout << subtraction(12) << "\n";
+
+	odd(20);
+
+
+	long number = 11;
+	cout << number << "! = " << factorial(number) << "\n";
+
 	return 0;
+}
+
+
+void odd(int x)
+{
+	if((x%2)!=0)
+	{
+		cout << "It is odd.\n";
+	}
+	else
+	{
+		cout << "It is even.\n";
+	}
 }
